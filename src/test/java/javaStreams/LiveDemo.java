b07c94114a7f680e8a7aa80afc4bb27c78c94847
DESKTOP-6T8U07T\User <mohamed.hamdy.issa@gmail.com>
2022-08-09 11:37:28 +0200

    build custom selenium methods using streams mapper\

---
+++ src/test/java/javaStreams/LiveDemo.java
@@ -0,0 +1,35 @@
+package javaStreams;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class LiveDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
+		driver.findElement(By.xpath("//tr//th[1]")).click();
+
+		List<WebElement> elements = driver.findElements(By.xpath("//tr//td[1]"));
+
+		List<String> originalsNames = elements.stream().map(s -> s.getText()).collect(Collectors.toList());
+
+		List<String> sortedList = originalsNames .stream().sorted().collect(Collectors.toList());
+
+		sortedList.stream().forEach(s -> System.out.println(s));
+
+		Assert.assertEquals(originalsNames, sortedList);
+	}
+
+}
